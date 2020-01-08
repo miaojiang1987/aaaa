6c23e7b7950450de76aa47e770cc58e554fd3829
Miao Jiang <miaojiang1987@gmail.com>
2020-01-08 16:02:44 -0500

    Update LinkedList

--- LinkedList/node.py
+++ LinkedList/node.py
@@ -9,12 +9,19 @@ class Node:
     def __init__(self,val):
         self.val=val
         self.next=None
+    
+
+def print_list(head):
+    cur=head
+    result=""
+    while cur:
+        cur.next=        
+        
 
 
-
-head=Node(5)
-node1=Node(6)
-head.next=node1
-print(head.val)
-print(head.next.val)        
+#head=Node(5)
+#node1=Node(6)
+#head.next=node1
+#print(head.val)
+#print(head.next.val)        
         
---
+++ LinkedList/problem_1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  8 15:31:55 2020
+
+@author: m_jia
+"""
+
+from node import Node
+from node import print_list
+
+def remove_duplicate(head):
+    hashset=set()
+    prev=None
+    cur=head
+    while cur!=None:
+        value=cur.val
+        if value in hashset:
+            prev.next=cur.next
+        
+
+node1=Node(5)
+print_list(node1)
